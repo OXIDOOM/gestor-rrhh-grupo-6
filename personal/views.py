8b7d45a038098b7e897034193d380a66b511e8ee
OXIDOOM <geralxdee@gmail.com>
2026-08-28 12:29:22 -0400

    Protect listar_empleados view with login_required
    Added login requirement to the listar_empleados view and included employee data retrieval.

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Empleado
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Protegemos la vista para que solo usuarios con sesión iniciada puedan entrar
+@login_required 
+def listar_empleados(request):
+    empleados = Empleado.objects.all() # Consulta SQL encubierta: trae todos los registros de la tabla
+    contexto = {'lista_empleados': empleados} # Empaquetamos los datos en un diccionario
+    return render(request, 'listar.html', contexto) # Enviamos el paquete de datos al archivo HTML
