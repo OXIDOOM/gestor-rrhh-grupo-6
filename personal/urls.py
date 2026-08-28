7b0cbe3864fa2ab6ffe5389c32d4abf78938c1eb
OXIDOOM <geralxdee@gmail.com>
2026-08-28 12:31:07 -0400

    Add URL pattern for listar_empleados view

--- personal/urls.py
+++ personal/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from .views import listar_empleados
 
+# Conectamos la dirección URL '/empleados/' con la función de la vista
+urlpatterns = [
+    path('empleados/', listar_empleados, name='listar_empleados'),
+]
