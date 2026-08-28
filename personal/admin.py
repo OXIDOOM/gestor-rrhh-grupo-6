d7c4dd9577bc21584a701c3747613d7997916212
OXIDOOM <geralxdee@gmail.com>
2026-08-28 12:28:50 -0400

    Update admin.py

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Empleado
 
-# Register your models here.
+# Registramos el modelo para que sea visible e interactivo en el panel /admin/
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'cargo', 'salario_mensual', 'esta_activo') # Columnas visibles en la tabla
+    list_filter = ('esta_activo', 'cargo') # Filtros laterales derechos
+    search_fields = ('nombre_completo', 'cargo') # Barra de búsqueda superior
